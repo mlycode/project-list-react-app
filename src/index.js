ae719523dfa4f4b65130f87a360a74c386222e97
mlycode <m.ly@live.co.uk>
2019-01-26 12:12:50 +0000

    add redux and setup file structure

--- src/index.js
+++ src/index.js
@@ -4,7 +4,19 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import rootReducer from "./store/reducers/rootReducer";
+
+const store = createStore(rootReducer);
+
+const app = (
+    <Provider store={store}>
+        <App />
+    </Provider>
+)
+
+ReactDOM.render(app, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
