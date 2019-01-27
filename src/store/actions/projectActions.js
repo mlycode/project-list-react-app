9d6c7b8eaddbc02a1a6a0eb5d1573aea2e83424e
mlycode <m.ly@live.co.uk>
2019-01-27 14:10:59 +0000

    handle async code to create project and post to firebase db

--- src/store/actions/projectActions.js
+++ src/store/actions/projectActions.js
@@ -1,6 +1,18 @@
 export const createProject = (project) => {
     return (dispatch, getState, {getFirebase, getFirestore}) => {
         //async call to db
-        dispatch({type: "CREATE_PROJECT", project: project});
+        const firestore = getFirestore();//ref to firestore db
+        firestore.collection("projects").add({
+            ...project,
+            authorFirstName: "Michael",
+            authorLastName: "Jackson",
+            authorId: 5293,
+            createdAt: new Date()
+        }).then(() => {
+            dispatch({type: "CREATE_PROJECT", project: project});
+        }).catch((err) => {
+            dispatch({type: "CREATE_PROJECT_ERROR", err});
+        });
+        
     }
 };
